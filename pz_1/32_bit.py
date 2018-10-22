d253809982207d85f40fe49dc9c0d927b34613ff
owl <owl@owlmail.com>
2018-10-22 16:25:27 +0300

    1st pz done.

--- pz_1/32_bit.py
+++ pz_1/32_bit.py
@@ -1,3 +1,10 @@
-print(b'some string')#.encode())
+import time
 
-print(format(23, '08b'))
+open('')
+start = time.time()
+for i in range(2 ** 32):
+    # print('{} : {}\t---\t{}'.format(i, 2**32, bin(i)[2:].zfill(32)))
+    pass
+print('Elapsed time {}.'.format(time.time() - start))
+
+# Elapsed time 181.92647790908813.
